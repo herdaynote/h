76e6b734981e0a9db4eb5863d67aa1c65fc0a1ff
herdaynote <53218130+herdaynote@users.noreply.github.com>
2021-05-28 07:09:14 +0700

    Create n.js

---
+++ n.js
@@ -0,0 +1,40 @@
+const handleIntersection = (entries) => {
+    entries.map((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.src = entry.target.dataset.src
+            entry.target.removeAttribute("data-src")
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+    rootMargin: '0px 0px -80% 0px'
+})
+
+document.addEventListener('scroll', () => {
+    let lazyImages = document.querySelectorAll('img[data-src]')
+    lazyImages.forEach(lazy => observer.observe(lazy))
+})
+
+const morePost = (url) => {
+    const xhr = new XMLHttpRequest()
+    document.querySelector('#blog-pager').innerHTML = "<div class='loading'>Memuat artikel <span>.</span><span>.</span><span>.</span></div>"
+                      
+    xhr.onreadystatechange = () => {
+    if ((xhr.readyState == 4) && (xhr.status == 200)) {
+            const newHTML = (new DOMParser()).parseFromString(xhr.responseText, 'text/html')
+            const articles = newHTML.querySelectorAll('article')
+            const morePost = newHTML.querySelector('#blog-pager').innerHTML
+            
+            articles.forEach((article) => {
+                document.querySelector('#content').append(article)
+            })
+            
+            document.querySelector('#blog-pager').innerHTML = morePost
+        }
+    }
+
+    xhr.open('GET', url, true)
+    xhr.send()
+}
